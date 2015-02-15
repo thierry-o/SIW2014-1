e5cadc749783d2dd0a83f8b38413153898ba5984
thierry-o <thierry.oddoudivers@laposte.net>
2015-02-14 21:50:03 -0400

    ajout effacement de la BDD lors de la suppression

--- app/views/suppFichier.php
+++ app/views/suppFichier.php
@@ -10,6 +10,14 @@ $fichier=Input::get('dossier')."/".Input::get('fichier');
 //suppression du fichier
 unlink($fichier);
 //echo $fichier;
+//récupération de l'id du fichier
+$id = DB::table('fichier')->select('fich_id')->where('fich_nom', Input::get('fichier'))->where('fich_chemin', Input::get('dossier'))->first();
+$num=intval($id->fich_id);
+//suppression de la base de données
+DB::table('partage')->where('part_fich_id', '=', $num)->delete();
+DB::table('fichier')->where('fich_id', '=', $num)->delete();
+//DB::delete('delete from partage')->where('fich_id', '==', '5');//$id->fich_id);
+//DB::delete('delete from fichier')->where('fich_id', '5');// $id->fich_id);
 echo "<h3>Le ficher a ete supprime</h3>";
 echo "<form action=\"appli\" method=\"get\">";
 echo '<input type="hidden" name="dir" value="'.Session::get('dossCourant').'" />';
